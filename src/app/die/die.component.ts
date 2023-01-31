29a18b2e4fa8f0ff6cf7954c6e166aeb7c107e5c
Nikola Savic <nikolasavic@fastmail.com>
2023-01-30 23:48:33 -0500

    Format

--- src/app/die/die.component.ts
+++ src/app/die/die.component.ts
@@ -22,7 +22,7 @@ export class DieComponent {
   holdToggled = new EventEmitter<number>();
 
   emitToggleHold() {
-    if(!this.paused) {
+    if (!this.paused) {
       this.hold = !this.hold;
       this.holdToggled.emit(this.position);
     }
