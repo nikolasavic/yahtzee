8b6e3e9d610b45c18ea0649579c4df06ed7a999a
Nikola Savic <nikolasavic@fastmail.com>
2023-01-30 23:33:06 -0500

    Refactor

--- src/app/die/die.component.spec.ts
+++ src/app/die/die.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+
 import { DieComponent } from './die.component';
 
 describe('DieComponent', () => {
   let component: DieComponent;
   let fixture: ComponentFixture<DieComponent>;
+  let nativeEl: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,19 +15,17 @@ describe('DieComponent', () => {
 
     fixture = TestBed.createComponent(DieComponent);
     component = fixture.componentInstance;
+    nativeEl = fixture.nativeElement;
+
     fixture.detectChanges();
   });
 
   it('default die value is 0', () => {
-    const imgElement = fixture.nativeElement;
-
     expect(component.value).toBe(0);
-    expect(imgElement.querySelector('img').src).toContain('assets/dice-0.svg');
+    expect(nativeEl.querySelector('img').src).toContain('assets/dice-0.svg');
   });
 
   it('default hold value is false', () => {
-    const imgElement = fixture.nativeElement;
-
     expect(component.hold).toBe(false);
   });
 
@@ -33,44 +33,44 @@ describe('DieComponent', () => {
     component.value = 5;
     fixture.detectChanges();
 
-    const imgElement = fixture.nativeElement;
-    expect(imgElement.querySelector('img').src).toContain('assets/dice-5.svg');
+    expect(nativeEl.querySelector('img').src).toContain('assets/dice-5.svg');
   });
 
   it('sets correct position based on position input', () => {
     component.position = 1;
     fixture.detectChanges();
 
-    const divElement = fixture.nativeElement;
-    expect(divElement.querySelector('div').classList).toContain('dice1');
+    expect(nativeEl.querySelector('div').classList).toContain('dice1');
   });
 
-  it('toggle hold when clicked', () => {
-    let die = fixture.debugElement.nativeElement.querySelector('img');
-    expect(component.hold).toBe(false);
+  describe('holds', () => {
+    it('toggle hold when clicked', () => {
+      let die = nativeEl.querySelector('img');
+      expect(component.hold).toBe(false);
 
-    die.click();
+      die.click();
 
-    expect(component.hold).toBe(true);
-  });
+      expect(component.hold).toBe(true);
+    });
 
-  it('emit holdToggled when clicked', () => {
-    const emitSpy = spyOn(component.holdToggled, 'emit');
+    it('emit holdToggled when clicked', () => {
+      const emitSpy = spyOn(component.holdToggled, 'emit');
 
-    component.emitToggleHold();
+      component.emitToggleHold();
 
-    expect(emitSpy).toHaveBeenCalled();
-  });
+      expect(emitSpy).toHaveBeenCalled();
+    });
 
-  it('applies hold css class when hold is true', () => {
-    let die = fixture.debugElement.nativeElement.querySelector('img');
-    expect(component.hold).toBe(false);
-    expect(die.classList).not.toContain('hold');
+    it('applies hold css class when hold is true', () => {
+      let die = nativeEl.querySelector('img');
+      expect(component.hold).toBe(false);
+      expect(die.classList).not.toContain('hold');
 
-    component.hold = true;
-    fixture.detectChanges();
+      component.hold = true;
+      fixture.detectChanges();
 
-    expect(component.hold).toBe(true);
-    expect(die.classList).toContain('hold');
+      expect(component.hold).toBe(true);
+      expect(die.classList).toContain('hold');
+    });
   });
 });
