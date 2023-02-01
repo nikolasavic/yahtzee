2013d33fa0b627922bf9e36c1b211c4962d8fc84
Nikola Savic <nikolasavic@fastmail.com>
2023-01-31 21:51:10 -0500

    Add svgs to roll count

--- src/app/roll-count/roll-count.component.spec.ts
+++ src/app/roll-count/roll-count.component.spec.ts
@@ -5,6 +5,7 @@ import { RollCountComponent } from './roll-count.component';
 describe('RollCountComponent', () => {
   let component: RollCountComponent;
   let fixture: ComponentFixture<RollCountComponent>;
+  let nativeEl: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,10 +14,11 @@ describe('RollCountComponent', () => {
 
     fixture = TestBed.createComponent(RollCountComponent);
     component = fixture.componentInstance;
+    nativeEl = fixture.nativeElement;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('render 3 indicator circles', () => {
+    expect(nativeEl.querySelectorAll('img').length).toBe(3);
   });
 });
