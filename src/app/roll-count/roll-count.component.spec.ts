79c408d8861e9b243b8e57cab001e7d0b09eee7b
Nikola Savic <nikolasavic@fastmail.com>
2023-02-03 22:24:27 -0500

    Roll count displays correct indicator after inputs

--- src/app/roll-count/roll-count.component.spec.ts
+++ src/app/roll-count/roll-count.component.spec.ts
@@ -1,3 +1,4 @@
+import { SimpleChange } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RollCountComponent } from './roll-count.component';
@@ -20,5 +21,52 @@ describe('RollCountComponent', () => {
 
   it('render 3 indicator circles', () => {
     expect(nativeEl.querySelectorAll('img').length).toBe(3);
+  });
+
+  it('default is unfilled', () => {
+    let nodeList = nativeEl.querySelectorAll('img');
+
+    expect(nodeList[0].src).toContain('assets/unfilledCircle.svg');
+    expect(nodeList[1].src).toContain('assets/unfilledCircle.svg');
+    expect(nodeList[2].src).toContain('assets/unfilledCircle.svg');
+  });
+
+  it('renders after first roll', () => {
+    component.round = 1;
+    component.ngOnChanges({
+      round: new SimpleChange(0, 1, false),
+    });
+    fixture.detectChanges();
+    let nodeList = nativeEl.querySelectorAll('img');
+
+    expect(nodeList[0].src).toContain('assets/filledCircle.svg');
+    expect(nodeList[1].src).toContain('assets/unfilledCircle.svg');
+    expect(nodeList[2].src).toContain('assets/unfilledCircle.svg');
+  });
+
+  it('renders after second roll', () => {
+    component.round = 2;
+    component.ngOnChanges({
+      round: new SimpleChange(1, 2, false),
+    });
+    fixture.detectChanges();
+    let nodeList = nativeEl.querySelectorAll('img');
+
+    expect(nodeList[0].src).toContain('assets/filledCircle.svg');
+    expect(nodeList[1].src).toContain('assets/filledCircle.svg');
+    expect(nodeList[2].src).toContain('assets/unfilledCircle.svg');
+  });
+
+  it('renders after third roll', () => {
+    component.round = 3;
+    component.ngOnChanges({
+      round: new SimpleChange(2, 3, false),
+    });
+    fixture.detectChanges();
+    let nodeList = nativeEl.querySelectorAll('img');
+
+    expect(nodeList[0].src).toContain('assets/filledCircle.svg');
+    expect(nodeList[1].src).toContain('assets/filledCircle.svg');
+    expect(nodeList[2].src).toContain('assets/filledCircle.svg');
   });
 });
--- src/app/roll-count/roll-count.component.ts
+++ src/app/roll-count/roll-count.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-roll-count',
   templateUrl: './roll-count.component.html',
   styleUrls: ['./roll-count.component.css'],
 })
-export class RollCountComponent {}
+export class RollCountComponent implements OnChanges {
+  @Input()
+  round: number = 0;
+
+  filled: boolean[] = [false, false, false];
+
+  ngOnChanges(changes: SimpleChanges) {
+    switch (this.round) {
+      case 0:
+        this.filled = [false, false, false];
+        break;
+      case 1:
+        this.filled = [true, false, false];
+        break;
+      case 2:
+        this.filled = [true, true, false];
+        break;
+      case 3:
+        this.filled = [true, true, true];
+        break;
+      default:
+        this.filled = [false, false, false];
+        break;
+    }
+  }
+
+  setCircleType(isFilled: boolean) {
+    return isFilled ? 'filled' : 'unfilled';
+  }
+}
