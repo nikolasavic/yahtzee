69d0f55677dd85c0e0e7f18ceca8edc36c586c4e
Nikola Savic <nikolasavic@fastmail.com>
2023-03-04 20:36:14 -0500

    GameControllerService subscribes to GameStateService.scoreSheet$

--- src/services/game-controller.service.spec.ts
+++ src/services/game-controller.service.spec.ts
@@ -1,12 +1,28 @@
 import { TestBed } from '@angular/core/testing';
+import { BehaviorSubject, Subscription } from 'rxjs';
 
 import { GameControllerService } from './game-controller.service';
+import { GameStateService } from './game-state.service';
+import { ScoreSheet } from '../app/score-sheet';
 
 describe('GameControllerService', () => {
   let service: GameControllerService;
+  let gameStateServiceStub = {
+    scoreSheet$: new BehaviorSubject<ScoreSheet>(
+      new ScoreSheet({
+        fours: 4,
+        fives: 5,
+        sixes: 6,
+      })
+    ),
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: GameStateService, useValue: gameStateServiceStub },
+      ],
+    });
     service = TestBed.inject(GameControllerService);
   });
 
@@ -17,6 +33,22 @@ describe('GameControllerService', () => {
 
     it('start game with dice roll', () => {
       expect(service.isScoringPhase).toBe(false);
+    });
+  });
+
+  describe('score sheet', () => {
+    it('subscribes and saves game state', () => {
+      expect(service.scores.aces).toBe(undefined);
+      expect(service.scores.sixes).toBe(6);
+    });
+
+    it('unsubscribe on ngOnDestroy', () => {
+      service.scoreSubscription = new Subscription();
+      spyOn(service.scoreSubscription, 'unsubscribe');
+
+      service.ngOnDestroy();
+
+      expect(service.scoreSubscription.unsubscribe).toHaveBeenCalledTimes(1);
     });
   });
 });
--- src/services/game-controller.service.ts
+++ src/services/game-controller.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Category } from '../app/category';
+import { ScoreSheet } from '../app/score-sheet';
+import { GameStateService } from './game-state.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameControllerService {
-  constructor() {}
+  constructor(private state: GameStateService) {
+    this.scoreSubscription = state.scoreSheet$.subscribe((scoreSheet) => {
+      this.scores = scoreSheet;
+    });
+  }
+
+  scores: ScoreSheet = new ScoreSheet({});
+  scoreSubscription: Subscription;
 
   round: number = 1;
   isScoringPhase: boolean = false;
 
   recordScore(category: Category, score: number) {}
+
+  ngOnDestroy() {
+    this.scoreSubscription.unsubscribe();
+  }
 }
